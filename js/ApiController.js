862a8a919ed866477a706c9e2cadfbaead0a5cb9
Olivier Girardot <ogmusik@gmail.com>
2020-10-02 17:25:04 +0200

    commiting the main app that will be cqlling the api for my parentheses checker just to test if it may work hosted on heroku

---
+++ js/ApiController.js
@@ -0,0 +1,29 @@
+function ApiController(ApiService) {
+    // API = '//localhost:3000/parentheses';
+    API = 'https://parentheses-api.herokuapp.com/parentheses';
+    this.start = false;
+    let ctrl = this;
+    this.entry = "";
+    this.result = {};
+    this.getUser = () => {
+        // if (!this.entry) {
+        //     return;
+        // }
+        ApiService
+            .getUser(this.entry)
+            .then(function(response) {
+                ctrl.result = response;
+            })
+        this.start = true;
+    };
+
+    this.clear = () => {
+        ctrl.result = null;
+        this.entry = null;
+        this.start = false;
+    }
+}
+
+angular
+    .module('app')
+    .controller('ApiController', ApiController);
